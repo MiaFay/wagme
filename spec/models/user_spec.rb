ca5a3fe11d7e5934b8abef5d311a0f536faa1b5c
Mia Buckland <Fayssoux@mias-MacBook-Pro.local>
2016-07-19 12:52:43 -0400

    hound error edit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,14 @@ require 'shoulda-matchers'
 require 'valid_attribute'
 
 describe User do
-  it { should have_valid(:username).when('user1', 'user2') }
-  it { should_not have_valid(:username).when(nil, '') }
 
-end
+  it { should have_valid(:email).when('user@example.com', 'another@example.com') }
+  it { should_not have_valid(:email).when(nil, '') }
+
+  it 'has matching password confirmation for the password' do
+    user = User.new
+    user.password = 'password'
+    user.password_confirmation = 'anotherpassword'
+
+    expect(page)
+  end
