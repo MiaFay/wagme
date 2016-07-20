8f35bd1406353e68bcc7b03ef66f9facad4fd203
Mia Buckland <Fayssoux@mias-MacBook-Pro.local>
2016-07-20 17:33:21 -0400

    resolved conflicts

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,10 +16,9 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-
   RSpec.configure do |config|
     config.include Devise::Test::ControllerHelpers, type: :controller
     config.include Devise::Test::ControllerHelpers, type: :view
   end
-  
+
 end
