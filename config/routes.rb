151f51ff404ac20b8bd72c509f9611ecb10a678c
Mia Buckland <Fayssoux@mias-MacBook-Pro.local>
2016-07-21 21:41:36 -0400

    edit root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :meetups
 
-  root to: "users#index"
+  root to: "meetups#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
